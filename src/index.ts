5927009acbce5f27f72c0876fc63643aedf0e7f6
unknown <you@example.com>
2019-06-07 15:39:33 +0800

    p

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { pipe } from 'ramda'
 import { processHeaders } from './helpers/headers'
 
-export function axios (config: AxiosRequestConfig) {
-	pipe(
+export function axios (config: AxiosRequestConfig): AxiosPromise {
+	return pipe(
 		transformConfig,
-		xhr,
+		(config) => xhr(config).then(res => transformResponseData(res)),
 	)(config)
 }
 
@@ -38,6 +38,13 @@ function transformRequestData (config: AxiosRequestConfig): AxiosRequestConfig {
 function transformHeaders (config: AxiosRequestConfig): AxiosRequestConfig {
 	const { headers = {}, data } = config
 	return processHeaders(headers, data)
+}
+
+function transformResponseData (res: AxiosResponse) {
+	return {
+		...res,
+		data: transformResponse(res.data),
+	}
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,12 +1,38 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
+export default function xhr (config: AxiosRequestConfig): AxiosPromise {
+    return new Promise((resolve, reject) => {
+        const { headers, data = null, url, method = 'get', responseType } = config
+        const request = new XMLHttpRequest()
 
-export default function xhr (config: AxiosRequestConfig) {
-    const { headers, data = null, url, method = 'get' } = config
-    const request = new XMLHttpRequest()
-    request.open(method.toUpperCase(), url, true)
-    Object.keys(headers).forEach(v => {
-        request.setRequestHeader(v, headers[v])
+        if (responseType) {
+            request.responseType = responseType
+        }
+
+        request.open(method.toUpperCase(), url, true)
+
+        request.onreadystatechange = () => {
+            if (request.readyState !== 4) {
+                return
+            }
+            const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+            const responseData = responseType !== 'text' ? request.response: request.responseText
+            const response: AxiosResponse = {
+                data: responseData,
+                status: request.status,
+                statusText: request.statusText,
+                headers: responseHeaders,
+                config,
+                request,
+            }
+            resolve(response)
+        }
+
+        Object.keys(headers).forEach(v => {
+            request.setRequestHeader(v, headers[v])
+        })
+        request.send(data)
     })
-    request.send(data)
+    
 }
